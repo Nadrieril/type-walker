483dcfa474a5a4523bf05dfc1d3aeace67c1d1df
Nadrieril <nadrieril+git@gmail.com>
2024-06-15 17:51:13 +0200

    Improve ast example

--- tests/ast.rs
+++ tests/ast.rs
@@ -1,6 +1,8 @@
 #![feature(impl_trait_in_assoc_type)]
 #![feature(box_patterns)]
 
+use std::fmt::Display;
+
 use type_walker::{
     lending_iterator::{Either, LendingIterator},
     walker::{empty_walker, InnerWalkable, TypeWalker, Walkable},
@@ -24,6 +26,14 @@ impl Expr {
         Expr::Mul(Box::new(self), Box::new(other))
     }
 
+    fn eval(&self) -> u64 {
+        match self {
+            Expr::Add(a, b) => a.eval() + b.eval(),
+            Expr::Mul(a, b) => a.eval() * b.eval(),
+            Expr::Num(n) => *n,
+        }
+    }
+
     /// Distribute multiplication over addition.
     fn distribute(&mut self) {
         use Expr::*;
@@ -41,6 +51,46 @@ impl Expr {
                 a.mul(m.clone()).add(b.mul(m))
             }),
             _ => {}
+        }
+    }
+}
+
+impl Display for Expr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        enum ParentNode {
+            Add,
+            Mul,
+        }
+        struct With<'a>(&'a Expr, ParentNode);
+        impl Display for With<'_> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let needs_parens = match self {
+                    With(Expr::Add(..), ParentNode::Mul) => true,
+                    With(Expr::Mul(..), ParentNode::Add) => false,
+                    _ => false,
+                };
+                if needs_parens {
+                    write!(f, "({})", self.0)
+                } else {
+                    write!(f, "{}", self.0)
+                }
+            }
+        }
+
+        match self {
+            Expr::Add(a, b) => write!(
+                f,
+                "{} + {}",
+                With(a, ParentNode::Add),
+                With(b, ParentNode::Add)
+            ),
+            Expr::Mul(a, b) => write!(
+                f,
+                "{} * {}",
+                With(a, ParentNode::Mul),
+                With(b, ParentNode::Mul)
+            ),
+            Expr::Num(n) => write!(f, "{n}"),
         }
     }
 }
@@ -65,14 +115,14 @@ impl Walkable for Expr {
 #[test]
 fn test_ast() {
     let num = |x| Expr::num(x);
-    let mut tree = num(0).mul(num(1).add(num(2).add(num(3))));
+    let mut tree = num(42).mul(num(1).add(num(2).add(num(3))));
+    assert_eq!(tree.eval(), 252);
+    assert_eq!(tree.to_string(), "42 * (1 + 2 + 3)");
+
     tree.walk()
         .inspect_enter(Expr::distribute)
         .run_to_completion();
-    assert_eq!(
-        tree,
-        num(0)
-            .mul(num(1))
-            .add(num(0).mul(num(2)).add(num(0).mul(num(3))))
-    );
+
+    assert_eq!(tree.eval(), 252);
+    assert_eq!(tree.to_string(), "42 * 1 + 42 * 2 + 42 * 3");
 }
